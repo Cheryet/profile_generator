7dd8dfe6ff3a7d9a4571c78b436d47d1c4e0a380
Cheryet <C.heryet@outlook.com>
2022-10-25 15:41:47 -0600

    Completed survey.js

---
+++ survey.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's you name? Nicknames are also acceptable.", (answer1) => {
+  rl.question("What's an activity you like doing?", (answer2) => {
+    rl.question("What do you listen to while doing that?", (answer3) => {
+      rl.question("Which meal is your favourite? (ex. Breakfast, Lunch, Dinner)", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal?", (answer5) => {
+          rl.question("Which sport is your absolute favourite?", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at.", (answer7) => {
+
+              console.log(`${answer1} loves listening to ${answer3} while ${answer2}, eating ${answer5} for ${answer4}, would rather be playing ${answer6} than anything else right now, and is amazing at ${answer7}`);
+
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
